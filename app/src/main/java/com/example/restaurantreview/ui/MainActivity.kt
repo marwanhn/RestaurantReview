5b9eff33c0e53e845a93a4d92bf27be83518dd05
MarwanHN <marwanhasbunur@gmail.com>
2023-08-27 16:41:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/restaurantreview/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.restaurantreview.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restaurantreview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
